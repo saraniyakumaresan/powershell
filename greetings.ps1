00037c60ff8ac93a4c0d21ed91e4c3a496a8673f
saraniyakumaresan <119513623+saraniyakumaresan@users.noreply.github.com>
2023-06-16 17:48:39 +0530

    Update greetings.ps1

--- greetings.ps1
+++ greetings.ps1
@@ -1,2 +1,5 @@
+Write-Host "Parameter 2: $yourname"
+Write-Host "##gbStart##yourname##splitKeyValue##saran##gbEnd"
+
 # Greet the user
-Write-Host "Hello, $name! Welcome to PowerShell."
+Write-Host "Hello, $yourname! Welcome to PowerShell."
