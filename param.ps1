d2ae90e79aefba0b92bc3abf02421357414965bc
saraniyakumaresan <119513623+saraniyakumaresan@users.noreply.github.com>
2023-06-21 19:19:49 +0530

    Update param.ps1

--- param.ps1
+++ param.ps1
@@ -1,8 +1,8 @@
-param(
- $param3,
- $param4
+param (
+    $param3,
+    $param4
 )
 
-Write-Host"Parameter1:$param3"
-Write-Host"Parameter2:$param4"
-Write-Host"##gbStart##Appstore##splitKeyValue##TestingAppstore##gbEnd##"
+Write-Host "Parameter 1: $param3"
+Write-Host "Parameter 2: $param4"
+Write-Host "##gbStart##Appstore##splitKeyValue##TestingAppstore##gbEnd##"
