a9fec2f36d4caf083ed2e2c62bf9d6298d65514f
saraniyakumaresan <119513623+saraniyakumaresan@users.noreply.github.com>
2023-06-22 09:50:22 +0530

    Update vishnups.ps1

--- vishnups.ps1
+++ vishnups.ps1
@@ -1,3 +1,7 @@
+Param(
 [Parameter(Mandatory=$True,Position=1)]
-
-                [string]$installtype,
+  [string]$param3,
+[Parameter(Mandatory=$True,Position=1)]
+  [string]$param4
+    )
+Write-Host "$param3 and $param4"
