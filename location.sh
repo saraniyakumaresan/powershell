3a66c33c912290ddd6001674b5066f7382048d39
saraniyakumaresan <119513623+saraniyakumaresan@users.noreply.github.com>
2023-08-24 16:09:41 +0530

    Add files via upload

---
+++ location.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script displays the current date and time, as well as the current working directory.
+
+CurrentDateTime=$(date)
+echo "Current date and time: $CurrentDateTime"
+
+CurrentWorkingDirectory=$(pwd)
+echo "Current working directory: $CurrentWorkingDirectory"
